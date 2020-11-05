2b3f21cefbac761b064f6957dd2598c4bba03674
sourabhmandal <19mandal97@gmail.com>
2020-11-05 20:27:45 +0530

    Added aHero component for mainpage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import SidenavBar from './Components/SideNavBar/SidenavBar'
+import Hero from './Components/HeroImg/Hero'
 import {ViewPort, Left} from 'react-spaces';
 function App() {
   return (
     <div className="App">
       <ViewPort>
-        <Left size={60} style={{backgroundColor:"#111817"}}>
+        <Left size={60} style={{backgroundColor:"#111817"}} order={1}>
           <SidenavBar/>
+        </Left>
+        <Left size={500} order={2} style={{backgroundColor:"#3fefee"}}>
+          <Hero/>
         </Left>
       </ViewPort>
     </div>
