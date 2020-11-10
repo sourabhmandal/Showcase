fd28c6e923f381fe7bb331bc4f3dc609246f0bdd
sourabhmandal <19mandal97@gmail.com>
2020-11-10 20:51:13 +0530

    Host config added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,7 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'v1x!l5vabs1rshd!n(42bn_^k$=&^+z701@b76yg@noyo499yl'
-
+SECURE_HSTS_SECONDS = 518400
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = False
+SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 CORS_ORIGIN_ALLOW_ALL = True
